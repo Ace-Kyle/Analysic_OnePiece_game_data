870501682863008f408e28a69fd1da9fba3e00ae
TinyFox <106698391+Ace-Kyle@users.noreply.github.com>
2025-04-09 21:32:41 +0700

    feat: Support get list of league out of class

--- opbr/extract/league.js
+++ opbr/extract/league.js
@@ -1,12 +1,14 @@
 import JSON_DATA from "../data/json_data.js";
 
-class League{
+export default class League{
 
     constructor(id, name) {
         this.id = id
         this.name = name
     }
-    static listOfLeagues = []
+    static #listOfLeagues = []
+    //don't change order of static block code vs above code.
+    // It ensures listOfLeagues is always created before init() called
     static {
         this.#init()
     }
@@ -17,15 +19,21 @@ class League{
 
 
     static getLeagueById(id){
-       if (this.listOfLeagues.length === 0) this.#init();
-       return this.listOfLeagues.find(league => league.id === id);
+       if (this.#listOfLeagues.length === 0) this.#init();
+       return this.#listOfLeagues.find(league => league.id === id);
     }
+
+    // getter
     id(){ return this.id; }
     name(){ return this.name; }
+    static getListOfLeagues(){
+        if (this.#listOfLeagues.length === 0) League.#init();
+        return this.#listOfLeagues;
+    }
 
     //avoid iterate list of league from raw data many times
     static #init(){
-        if (this.listOfLeagues.length > 0) return;
+        if (this.#listOfLeagues.length > 0) return;
 
         const LEAGUES = JSON_DATA.listOf(JSON_DATA.TYPE.LEAGUE)
         let list = []
@@ -35,10 +43,12 @@ class League{
             name = rank['league_name'];
             list.push(new League(id, name));
         }
-        this.listOfLeagues = list;
+        this.#listOfLeagues = list;
     }
 
 }
 
 //test
-console.log(League.getLeagueById(100))
+let list = League.getListOfLeagues()
+console.log(list);
+console.log(list.map( ({id, name, counter=0}) => ({id, name, counter})))

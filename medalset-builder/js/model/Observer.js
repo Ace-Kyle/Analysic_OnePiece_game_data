ff1a6cbfe16283858bf71818460634566782e16d
TinyFox <galaxyeyekaizer@gmail.com>
2025-07-03 10:15:05 +0700

    chore: Remove exception for JSONversion.md from .gitignore

--- medalset-builder/js/model/Observer.js
+++
@@ -1,9 +0,0 @@
-export default class Observer {
-    constructor() {
-    }
-    //support Observer pattern
-    update(data) {
-        // This method should be overridden by subclasses
-        console.warn('Observer update method not implemented.');
-    }
-}
